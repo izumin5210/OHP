289edab9656d62072bcc27772c8e74ceacb3ee8b
izumin5210 <masayuki@izumin.info>
2017-05-04 14:18:02 +0900

    Introduce react-ace

--- src/containers/EditorContainer.js
+++ src/containers/EditorContainer.js
@@ -1,15 +1,38 @@
 // @flow
 import { PureComponent } from 'react'
+import AceEditor from 'react-ace'
+import 'brace/mode/markdown'
+import 'brace/theme/tomorrow'
+import 'brace/keybinding/vim'
 
 type Props = {
 }
 
-export default class EditorContainer extends PureComponent<void, Props, void> {
+type State = {
+  body: string,
+}
+
+export default class EditorContainer extends PureComponent<void, Props, State> {
+  constructor (props: Props) {
+    super(props)
+    this.state = {
+      body: '',
+    }
+  }
+
+  state: State
+
   render () {
     return (
-      <div>
-        <h2>Editor</h2>
-      </div>
+      <AceEditor
+        mode='markdown'
+        theme='tomorrow'
+        keyboardHandler='vim'
+        width='100%'
+        height='100%'
+        value={this.state.body}
+        onChange={v => this.setState({ body: v })}
+      />
     )
   }
 }
