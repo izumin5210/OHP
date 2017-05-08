dd09166a2c66d9b5b7be0c9901a096ecc11f1c31
izumin5210 <masayuki@izumin.info>
2017-05-08 23:06:11 +0900

    Introduce react-router and react-router-redux

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,14 +1,24 @@
 // @flow
 import createSagaMiddleware, { END } from 'redux-saga'
+import {
+  ConnectedRouter as Router,
+  routerMiddleware as createRouterMiddleware,
+} from 'react-router-redux'
+import createHistory from 'history/createHashHistory'
 
 import run from 'framework/run'
 
 import { app as configureStore } from 'store/configure'
 import sagas from 'store/sagas'
-import CreatorContainer from 'containers/CreatorContainer'
 
+import { app as routes } from 'routes'
+
+const history = createHistory()
+const routerMiddleware = createRouterMiddleware(history)
 const sagaMiddleware = createSagaMiddleware()
-const store = configureStore({}, sagaMiddleware)
+
+const store = configureStore({}, sagaMiddleware, routerMiddleware)
+
 // $FlowFixMe
 store.runSaga = sagaMiddleware.run
 // $FlowFixMe
@@ -17,10 +27,15 @@ store.close = () => store.dispatch(END)
 store.runSaga(sagas)
 
 const containerElement = document.getElementById('container')
+const component = (
+  <Router {...{ history }}>
+    { routes }
+  </Router>
+)
 
 if (containerElement != null) {
   run(
-    <CreatorContainer />,
+    component,
     containerElement,
     store,
   )
