4eb0afa56ddfad2aea79521cf3fabc290c0ad6dd
izumin5210 <masayuki@izumin.info>
2017-05-08 22:32:59 +0900

    Introduce redux-saga

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,10 +1,21 @@
 // @flow
-import run from 'framework/run'
-import { app as configureStore } from 'store/configure'
+import createSagaMiddleware, { END } from 'redux-saga'
 
+import run from 'framework/run'
+
+import { app as configureStore } from 'store/configure'
+import sagas from 'store/sagas'
 import CreatorContainer from 'containers/CreatorContainer'
 
-const store = configureStore({})
+const sagaMiddleware = createSagaMiddleware()
+const store = configureStore({}, sagaMiddleware)
+// $FlowFixMe
+store.runSaga = sagaMiddleware.run
+// $FlowFixMe
+store.close = () => store.dispatch(END)
+// $FlowFixMe
+store.runSaga(sagas)
+
 const containerElement = document.getElementById('container')
 
 if (containerElement != null) {
---
+++ src/store/sagas/index.js
@@ -0,0 +1,7 @@
+// @flow
+import type { IOEffect } from 'redux-saga/effects'
+
+export default function * (): Generator<IOEffect, *, *> {
+  yield [
+  ]
+}
