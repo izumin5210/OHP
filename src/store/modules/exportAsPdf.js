5ce58c53dba0cf940dcb200ddab052e5cc38f29d
izumin5210 <masayuki@izumin.info>
2017-05-09 01:33:13 +0900

    Add exportAsPdf state and start action

--- src/store/modules/exportAsPdf.js
+++ src/store/modules/exportAsPdf.js
@@ -1,6 +1,38 @@
 /* @flow */
-import { createAction } from 'redux-actions'
+import { createAction, handleActions } from 'redux-actions'
+import { Record } from 'immutable'
 
-/* ======== Actions ======= */
+/* ======= Types ======= */
+
+export type FetchStatus = 'none' | 'loading' | 'loaded' | 'failed'
+export type ExportAsPdfStateConfig = {
+  fetchStatus: FetchStatus,
+}
+
+const defaultValue: ExportAsPdfStateConfig = {
+  fetchStatus: 'none',
+}
+
+export class ExportAsPdfState extends Record(defaultValue) {
+  // eslint-disable-next-line no-useless-constructor
+  constructor (values: $Shape<ExportAsPdfStateConfig>) {
+    super(values)
+  }
+
+  entity: FetchStatus
+}
+
+const initialState = new ExportAsPdfState()
+
+/* ======= Actions ======= */
 
 export const prepare = createAction('exportAsPdf:prepare')
+export const start = createAction('exportAsPdf:start')
+
+/* ======= Reducer ======= */
+
+export default handleActions({
+  [prepare.toString()]: (state: ExportAsPdfState) => {
+    return state.set('fetchStatus', 'loading')
+  },
+}, initialState)
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -5,13 +5,16 @@ import { routerReducer } from 'react-router-redux'
 import type { Location } from 'react-router'
 
 import entities from './entities'
+import exportAsPdf from './exportAsPdf'
 
 export type RootState = {
   entities: typeof entities,
+  exportAsPdf: typeof exportAsPdf,
   router: ?Location,
 }
 
 export default combineReducers({
   entities,
+  exportAsPdf,
   router: routerReducer,
 })
