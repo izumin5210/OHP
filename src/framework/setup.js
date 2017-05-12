22e7a107b1d1a18dd0ca63e5e604930df8d288e6
izumin5210 <masayuki@izumin.info>
2017-05-12 18:56:47 +0900

    Introduce react-addons-perf

--- src/framework/setup.js
+++ src/framework/setup.js
@@ -5,6 +5,7 @@ global.React = React
 
 if (process.env.NODE_ENV !== 'production') {
   global.assert = require('power-assert')
+  global.Perf = require('react-addons-perf')
 }
 
 if (global.Promise == null) {
