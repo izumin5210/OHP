04e6f7ec2907da4534f731eb806369d87bda8739
izumin5210 <masayuki@izumin.info>
2017-05-16 23:10:39 +0900

    Set previewState.width on resize SlidePreview

--- src/components/preview/SlidePreview.js
+++ src/components/preview/SlidePreview.js
@@ -1,20 +1,30 @@
 // @flow
 import { PureComponent } from 'react'
+import Measure from 'react-measure'
+
+import type { Dimension } from 'react-measure'
 
 import styles from './SlidePreview.css'
 
 type Props = {
   bodyElement: React$Element<*>,
+  setWidth: (width: number) => any,
 }
 
 export default class SlidePreview extends PureComponent<void, Props, void> {
   // for lint
   props: Props
 
+  onMeasure = ({ width }: Dimension) => {
+    this.props.setWidth(width)
+  }
+
   render () {
     return (
       <div className={styles.pages} >
-        { this.props.bodyElement }
+        <Measure onMeasure={this.onMeasure}>
+          { this.props.bodyElement }
+        </Measure>
       </div>
     )
   }
--- src/containers/PreviewContainer.js
+++ src/containers/PreviewContainer.js
@@ -1,9 +1,13 @@
 // @flow
 import { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import debounce from 'lodash/debounce'
 
+import type { Dispatch } from 'redux'
+import type { Action } from 'redux-actions'
 import type { Connector } from 'react-redux'
 
+import { setWidth } from 'store/modules/previewState'
 import { getBodyAst } from 'store/selectors/processor'
 import { SlidePreview } from 'components/preview'
 
@@ -14,6 +18,7 @@ type RequiredProps = {
 
 type InjectedProps = {
   bodyElement: React$Element<*>,
+  setWidth: (width: number) => any,
 }
 
 type Props = RequiredProps & InjectedProps
@@ -22,6 +27,13 @@ const connector: Connector< RequiredProps, Props> = connect(
   (state: RootState) => ({
     bodyElement: getBodyAst(state).contents,
   }),
+  (dispatch: Dispatch<Action<any, any>>) => ({
+    setWidth: debounce(
+      (width: number) => dispatch(setWidth(width)),
+      300,
+      { maxWait: 1000 },
+    )
+  }),
 )
 
 class PreviewContainer extends PureComponent<void, Props, void> {
@@ -29,9 +41,9 @@ class PreviewContainer extends PureComponent<void, Props, void> {
   props: Props
 
   render () {
-    const { bodyElement } = this.props
+    const { bodyElement, setWidth } = this.props
     return (
-      <SlidePreview {...{ bodyElement }} />
+      <SlidePreview {...{ bodyElement, setWidth }} />
     )
   }
 }
