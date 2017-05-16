4dc2b3d62a8bf2f8ebff2f0a21a998c5b5cf5f9a
izumin5210 <masayuki@izumin.info>
2017-05-16 23:10:23 +0900

    Add previewState for storing slide preview width

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -7,15 +7,18 @@ import type { Location } from 'react-router'
 import entities from './entities'
 import exportAsPdf from './exportAsPdf'
 import editorState from './editorState'
+import previewState from './previewState'
 
 import type { EntitiesState } from './entities'
 import type { ExportAsPdfState } from './exportAsPdf'
 import type { EditorState } from './editorState'
+import type { PreviewState } from './previewState'
 
 export type RootState = {
   entities: EntitiesState,
   exportAsPdf: ExportAsPdfState,
   editorState: EditorState,
+  previewState: PreviewState,
   router: ?Location,
 }
 
@@ -23,5 +26,6 @@ export default combineReducers({
   entities,
   exportAsPdf,
   editorState,
+  previewState,
   router: routerReducer,
 })
