3ff1f3f7316faf69979e1caeaf49b556d133fd7b
izumin5210 <masayuki@izumin.info>
2017-05-24 20:46:30 +0900

    Remove inline DevTools

--- src/framework/Root.js
+++ src/framework/Root.js
@@ -4,11 +4,6 @@ import { Provider } from 'react-redux'
 import type { Element } from 'react'
 import type { Store } from 'redux'
 
-let DevTools = null
-if (process.env.NODE_ENV !== 'production') {
-  DevTools = require('./DevTools').default
-}
-
 type Props = {
   element: Element<*>,
   store: Store<*, *>,
@@ -19,7 +14,6 @@ export default function Root ({ element, store }: Props) {
     <Provider {...{ store }}>
       <div className='Root'>
         { element }
-        { DevTools != null && <DevTools /> }
       </div>
     </Provider>
   )
