8aff052028247cbebf2b2a03ee5e3fb9958a1c47
izumin5210 <masayuki@izumin.info>
2017-06-19 23:21:41 +0900

    Inject global styles

---
+++ packages/OHP/src/styles/injectGlobalStyle.js
@@ -0,0 +1,13 @@
+// @flow
+import { injectGlobal } from 'styled-components'
+import normalizeCss from 'styles/vendor/normalize.css/normalize.css'
+
+export default function injectGlobalStyle () {
+  injectGlobal`${normalizeCss}`
+  injectGlobal`
+    html {
+      font-size: 14px;
+      font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Segoe UI", "Noto Sans Japanese", "ヒラギノ角ゴ ProN W3", Meiryo, sans-serif;
+    }
+  `
+}
